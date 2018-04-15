bfe51e6e26448c481c9adbeeda18794664132bc4
Alena <borovskih_av@mail.ru>
2018-04-15 23:42:05 +0500

    Third commit

--- 1edition.cs
+++ 1edition.cs
@@ -10,9 +10,6 @@ namespace Flags
     {
         public static long Solve(int numberOfStripes)
         {
-            long firstPart = 2;
-            long baseForTwoStripes = firstPart;
-            if (numberOfStripes == 1 || numberOfStripes == 2)
             {
                 return firstPart;
             }
